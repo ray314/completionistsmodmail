57c97a2234acf6e20ced31e90944f599b2dbca1e
Your Name <you@example.com>
2022-06-03 05:19:17 -0700

    initial configuration

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const { Intents, Client } = require("discord.js");
+const { Hooks } = require('./modules/hooks.js');
+const auth = require('./auth.json');
+const config = require('./config.json');
+
 const client = new Client({ intents: [
     Intents.FLAGS.GUILDS, 
-    Intents.FLAGS.GUILD_MESSAGES
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGES
 ] })
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity('minecraft ost', { type: 'LISTENING' });
+    Hooks(client);
+});
+
+client.login(auth.token);
