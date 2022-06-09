b193de0f4a1396287f4abfc357a4e26c3761d718
ray314 <yeungj1602@gmail.com>
2022-06-09 22:35:15 +1200

    updated status

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ partials: [
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity('minecraft ost', { type: 'LISTENING' });
+    client.user.setActivity('Bald eagle', { type: 'LISTENING' });
     Hooks(client);
 });
 
